f494ca9aa0bc83ecadbb90d85bb67fa3fec0867c
Adam Li <adam2392@gmail.com>
2018-01-21 01:15:51 +0000

    Adding all training and slurm submit files.

---
+++ main.py
@@ -0,0 +1,104 @@
+import sys
+# sys.path.append('./')
+
+import model.ieeg_cnn_rnn
+import model.train
+
+import processing.util as util
+import keras
+from keras.callbacks import ModelCheckpoint
+
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import scale
+
+if __name__ == '__main__':
+	traindatadir = '../traindata/'
+	tempdatadir = '../tempdata/'
+
+	traindatadir = str(sys.argv[1])
+	tempdatadir = str(sys.argv[2])
+
+	imsize=32
+	numfreqs = 4
+	ieegdnn = model.ieeg_cnn_rnn.IEEGdnn(imsize=imsize, n_colors=numfreqs)
+
+	# define data filepath to images
+	image_filepath = os.path.join(traindatadir, 'trainimages.npy')
+	images = np.load(image_filepath)
+
+	# load the ylabeled data
+	ylabel_filepath = os.path.join(traindatadir, 'trainlabels.npy')
+	ylabels = np.load(ylabel_filepath)
+
+	# checkpoint
+	filepath=os.path.join(tempdatadir,"weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5")
+	checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+
+	w_init = None
+	n_layers = (4,2,1)
+	poolsize = (2,2)
+	filtersize = (3,3)
+
+	size_fc = 1024
+	DROPOUT = False #True
+
+	# VGG-12 style later
+	vggcnn = ieegdnn._build_2dcnn(w_init=w_init, n_layers=n_layers, 
+	                              poolsize=poolsize, filter_size=filtersize)
+	vggcnn = ieegdnn._build_seq_output(vggcnn, size_fc, DROPOUT)
+	print(vggcnn.summary())
+
+	# load in CNN/LSTM
+	# num_timewins = 5
+	# size_mem = 128
+	# size_fc = 1024
+	# DROPOUT = False
+	# cnn_lstm = ieegdnn.build_cnn_lstm(num_timewins=num_timewins, 
+	#                                   size_mem=size_mem, size_fc=size_fc, DROPOUT=DROPOUT)
+	# print(cnn_lstm.get_shape)
+
+	# split into train,valid,test sets
+	datahandler = util.DataHandler()
+
+	images = scale(images, axis=0, with_mean=True, with_std=True, copy=True )
+
+	# format the data correctly 
+	# (X_train, y_train), (X_val, y_val), (X_test, y_test) = datahandler.reformatinput(images, labels)
+	X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.33, random_state=42)
+	X_train = X_train.astype("float32")
+	X_test = X_test.astype("float32")
+
+	loss = 'binary_crossentropy'
+	optimizer = keras.optimizers.Adam(lr=0.001, 
+	                                        beta_1=0.9, 
+	                                        beta_2=0.999,
+	                                        epsilon=1e-08,
+	                                        decay=0.0)
+	metrics = ['accuracy']
+	cnn_config = ieegdnn.compile_model(vggcnn, loss=loss, optimizer=optimizer, metrics=metrics)
+
+
+	NUM_EPOCHS = 100
+	batch_size = 32 # or 64... or 24
+	# construct the image generator for data augmentation and construct
+	# the set of callbacks
+	aug = keras.preprocessing.image.ImageDataGenerator(width_shift_range=0.1,
+	    height_shift_range=0.1, horizontal_flip=True,
+	    fill_mode="nearest")
+	# callbacks_list = [checkpoint]
+	callbacks = [checkpoint, keras.callbacks.LearningRateScheduler(model.train.poly_decay)]
+	INIT_LR = 5e-3
+	 
+	HH = vggcnn.fit_generator(
+	    aug.flow(X_train, y_train, batch_size=batch_size * G), # adds augmentation to data using generator
+	    validation_data=(X_test, y_test),  
+	    steps_per_epoch=len(X_train) // (batch_size * G),    #
+	    epochs=NUM_EPOCHS,
+	    callbacks=callbacks, verbose=2)
+
+	# save final history object
+	vggcnn.save('final_weights.h5')
+
+
---
+++ submit_train.sh
@@ -0,0 +1,83 @@
+#!/bin/bash -l
+
+##############################################################
+#
+# Shell script for submitting parallel python jobs on SLURM 
+# cluster with nodes, CPUS, tasks, GPUs
+#
+##############################################################
+# ml python
+# ml parallel
+
+## activate virtualenv/condaenv to use their modules
+
+# 1. Prompt user for input that runs the analysis
+echo "Begin analysis." # print beginning statement
+# NEED TO RUN FOR EZ=0,1,2,3 and varying PZ all once
+
+# Pause before running to check
+tempdatadir='/home-1/ali39@jhu.edu/data/fragility/_temp/tvbsim/'
+outputdatadir='/home-1/ali39@jhu.edu/data/fragility/output/tvbsim/'
+printf "\nThis is the data directories: \n"
+printf "Temp datadir: $tempdatadir \n"
+printf "Output datadir: $outputdatadir \n"
+printf "\n"
+
+#### Create all logging directories if needed
+# _gnuerr = all error logs for sbatch gnu runs %A.out 
+# _gnuout = all output logs for sbatch gnu runs %A.out 
+# _logs = the parallel gnu logfile for resuming job at errors 
+outdir=_out
+# create output directory 
+if [ -d "$outdir" ]; then  
+	echo "Out log directory exists!\n\n"
+else
+	mkdir $outdir
+fi
+
+# 2. Define Slurm Parameters
+NUM_PROCSPERNODE=24  	# number of processors per node (1-24). Use 24 for GNU jobs.
+NUM_NODES=1				# number of nodes to request
+NUM_CPUPERTASK=1
+
+partition=gpu 	# debug, shared, unlimited, parallel, gpu, lrgmem, scavenger
+# partition=debug
+qos=scavenger
+numgpus=4
+
+gpu="gpu:$numgpus"
+echo $gpu
+# set jobname
+jobname="submit_trainpy.log"
+
+# create export commands
+exvars="tempdatadir=${tempdatadir},\
+outputdatadir=${outputdatadir} "
+
+## job reqs
+if [[ "${modelType}" -eq 1 ]]; then
+	walltime=5:00:0
+else
+	walltime=5:00:0					# the walltime for each computationfi
+fi
+
+# build basic sbatch command with all params parametrized
+sbatcomm="sbatch \
+ --time=${walltime} \
+ --nodes=${NUM_NODES} \
+ --cpus-per-task=${NUM_CPUPERTASK} \
+ --job-name=${jobname} \
+ --ntasks-per-node=${NUM_PROCSPERNODE} \
+ --partition=${partition}
+ --gres=${gpu} "
+
+# build a scavenger job, gpu job, or other job
+printf "Sbatch should run now\n"
+
+echo $sbatcomm $exvars ./runtvbarrayjob.sbatch 
+
+${sbatcomm} --export=$exvars ./runtvbarrayjob.sbatch
+
+read -p "Continuing in 0.5 Seconds...." -t 0.5
+echo "Continuing ...."
+# grep for SLURM_EXPORT_ENV when testing
